1dc1c64546a5520f09b3998abc761b9d649dbd68
Bruno <bganontha@gmail.com>
2024-09-09 17:28:00 +0100

    Practicing models and migrations

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -6,3 +6,8 @@ class Products(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     price = models.IntegerField()
+
+class Huiles(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    price = models.IntegerField()
