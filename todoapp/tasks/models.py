6b180cfa7ab796eed4f37d79d25eef0a0c2d1710
Bruno <bganontha@gmail.com>
2024-09-20 14:10:51 +0100

    Practice project with todo app

---
+++ todoapp/tasks/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class todotask(models.Model):
+    
+    title = models.CharField(max_length=200)
+    description  = models.TextField(max_length=1000, default='task description...')
+    completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=20)
+    created = models.DateTimeField(auto_now_add=True)
