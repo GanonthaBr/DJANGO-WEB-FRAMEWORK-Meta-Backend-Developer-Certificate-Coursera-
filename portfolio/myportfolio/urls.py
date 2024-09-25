08580736f91dcefb88af77663244feadda1e3666
Bruno <bganontha@gmail.com>
2024-09-25 12:04:57 +0100

    Removed trailing slashes at the end of endpoints

--- portfolio/myportfolio/urls.py
+++ portfolio/myportfolio/urls.py
@@ -5,10 +5,10 @@ from .views import ProjectCreate, ProjectRetrieve,ProjectDetails, ProjectUpdate,
 
 urlpatterns = [
     path('',views.index, name="home"),
-    path('about/',views.about, name="about"),
-    path('projects/',ProjectRetrieve.as_view(), name="projects"),
+    path('about',views.about, name="about"),
+    path('projects',ProjectRetrieve.as_view(), name="projects"),
     path('project/<int:pk>',ProjectDetails.as_view(), name="project"),
     path('<int:pk>/update',ProjectUpdate.as_view(), name="update"),
     path('<int:pk>/delete',ProjectDelete.as_view(), name="delete"),
-    path('add_project/',ProjectCreate.as_view(), name="add_project"),
+    path('add_project',ProjectCreate.as_view(), name="add_project"),
 ]
