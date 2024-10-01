8ffae30f0c55ec01f30026b13148f6b33696362b
Bruno <bganontha@gmail.com>
2024-10-01 18:57:07 +0100

    Install DRF

--- BigLemon/BigLemonAPI/views.py
+++ BigLemon/BigLemonAPI/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
+@api_view(['GET'])
+# @api_view
+def books(request):
+    return Response('The book list',status=status.HTTP_200_OK)
