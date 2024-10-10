dd0a1a717b948f2e8c9c35fffb348c47cfc5c51e
Ganontha <bganontha@gmail.com>
2024-10-10 17:06:44 +0100

    class and function based views

--- BigLemon/BigLemonAPI/urls.py
+++ BigLemon/BigLemonAPI/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('books/',views.books),
+    path('books/<int:pk>',views.BookViews.as_view()),
+
 ]
--- BigLemon/BigLemonAPI/views.py
+++ BigLemon/BigLemonAPI/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework import status
 
 # Create your views here.
@@ -8,3 +9,10 @@ from rest_framework import status
 # @api_view
 def books(request):
     return Response('The book list',status=status.HTTP_200_OK)
+
+class BookViews(APIView):
+    def get(self, request, pk):
+        return Response({"message":"single book with id " + str(pk)}, status.HTTP_200_OK)
+    
+    def put(self, request, pk):
+        return Response({"title":request.data.get('title')}, status.HTTP_200_OK)
