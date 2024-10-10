a49705c7425c72b5138030bef01eed571add1dda
Ganontha <bganontha@gmail.com>
2024-10-10 17:27:37 +0100

    class based views: get and put

--- BigLemon/BigLemonAPI/urls.py
+++ BigLemon/BigLemonAPI/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/',views.books),
-    path('books/<int:pk>',views.BookViews.as_view()),
+    # path('books/',views.books),
+    path('books/<int:pk>',views.Book.as_view()),
+    path('books/',views.BookViews.as_view()),
 
 ]
--- BigLemon/BigLemonAPI/views.py
+++ BigLemon/BigLemonAPI/views.py
@@ -11,8 +11,14 @@ def books(request):
     return Response('The book list',status=status.HTTP_200_OK)
 
 class BookViews(APIView):
-    def get(self, request, pk):
-        return Response({"message":"single book with id " + str(pk)}, status.HTTP_200_OK)
-    
+    def get(self, request):
+        author = request.GET.get('author')
+        if(author):
+            return Response({"message":"List of the books written by: " + author}, status.HTTP_200_OK)
+        return Response({"message":"list of the books"}, status.HTTP_200_OK)
     def put(self, request, pk):
         return Response({"title":request.data.get('title')}, status.HTTP_200_OK)
+
+class Book(APIView):
+    def get(self, request,pk):
+        return Response({"message":"single book with id:" + str(pk)}, status.HTTP_200_OK)
